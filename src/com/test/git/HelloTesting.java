ea7f572d8270636b5faad9a07d30bbca3663b361
Rakesh Kumar-XFT <Rakesh Kumar-XFT@MCKINSEY.COM>
2017-04-14 22:22:49 +0530

    cmmit tst

---
+++ src/com/test/git/HelloTesting.java
@@ -0,0 +1,10 @@
+package com.test.git;
+
+public class HelloTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( ": Testin goging on......");
+	}
+
+}
