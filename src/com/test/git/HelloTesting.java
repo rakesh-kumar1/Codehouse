34ef59341cf093735775de6d99687a53b37ea98e
Rakesh Kumar-XFT <Rakesh Kumar-XFT@MCKINSEY.COM>
2017-04-19 08:26:42 +0530

    checking build

--- src/com/test/git/HelloTesting.java
+++ src/com/test/git/HelloTesting.java
@@ -5,6 +5,8 @@ public class HelloTesting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println( ": Testin goging on......");
+		System.out.println( ": Testin goging on...adsfadsf...");
+		System.out.println( ": Testin goging on...qewrewr...");
 	}
 
 }
